ccb0663505c507942d0499b077c2856483b8aabc
Victor <vmanzanilla7@gmail.com>
2023-05-17 20:43:16 -0500

    working on tabs

---
+++ tabs.js
@@ -0,0 +1,7 @@
+
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = tabList.querySelectorAll('[role="tab"]');
+
+tabList.addEventListener('keydown', (e) => {
+    console.log(e);
+})
