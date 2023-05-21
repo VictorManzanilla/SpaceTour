286da984b97018efd33618e2c4c14b518a69067c
Victor <vmanzanilla7@gmail.com>
2023-05-21 18:34:07 -0500

    working on tabs with js

--- tabs.js
+++ tabs.js
@@ -2,6 +2,9 @@
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
+// The numbers are the keycodes for the right and left keys. To that
+// do console.log(e.keycode)
 tabList.addEventListener('keydown', (e) => {
-    console.log(e);
+    const keydownLeft = 37;
+    const keydownRight = 39;
 })
